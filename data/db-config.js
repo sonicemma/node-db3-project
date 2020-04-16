d34760b3661e2fe13178738abd00069bfbd09098
emma <sonicjane2020@gmail.com>
2020-04-16 00:57:03 -0700

    hit project mpv

---
+++ data/db-config.js
@@ -0,0 +1,7 @@
+const db = knex(config.development);
+
+const knex = require("knex");
+
+const config = require("../knexfile");
+
+module.exports = db;
--- queries.sql
+++ queries.sql
@@ -4,6 +4,8 @@
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
+-- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
